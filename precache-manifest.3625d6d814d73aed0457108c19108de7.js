d49f15f50f1507b34b46dc225f8d3a5eaf963918
knzn <rdacles@gmail.com>
2020-01-13 16:56:16 +0800

    Updates

---
+++ precache-manifest.3625d6d814d73aed0457108c19108de7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c6d451016081562d63912669654f6b",
+    "url": "/robo-by-andrei/index.html"
+  },
+  {
+    "revision": "47c9981ca9436a6771d2",
+    "url": "/robo-by-andrei/static/css/main.7fabdb33.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/robo-by-andrei/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "47c9981ca9436a6771d2",
+    "url": "/robo-by-andrei/static/js/main.2d67298e.chunk.js"
+  },
+  {
+    "revision": "78a2e9c924233efd1f81",
+    "url": "/robo-by-andrei/static/js/runtime-main.f3db59cb.js"
+  }
+]);
